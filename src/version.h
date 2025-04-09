c5dfd4e43581977881d6b188cd6ed76a27a43fb3
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2025-04-08 20:20:05 -0400

    restored original format of keymap.c for readability

--- src/version.h
+++
@@ -1,9 +0,0 @@
-// just to stop warnings. actual is in qmk_firmware
-#include <stdbool.h>
-#define SAFE_RANGE 0
-#define RGB_MATRIX_LED_COUNT 0
-#define HSV int
-#define RGB int
-#define UINT8_MAX 0
-
-int hsv_to_rgb(int);
